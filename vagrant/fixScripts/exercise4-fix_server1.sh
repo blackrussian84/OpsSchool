57be730ee5a17bc85275644c714a3198892e79ad
Jenya <shwebsec@gmail.com>
2020-02-25 23:55:32 +0200

    fix etc hosts locatin of alias

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
-sudo chmod o+w /etc/hosts && echo '192.168.100.11 server2' >> /etc/hosts
+sudo chmod o+w /etc/hosts && sed -i  "3i 192.168.100.11 server2" /etc/hosts
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
-sudo chmod o+w /etc/hosts && echo "192.168.100.10 server1" >> /etc/hosts
+#sudo chmod o+w /etc/hosts && echo "192.168.100.10 server1" >> /etc/hosts
+sudo chmod o+w /etc/hosts && sed -i  "3i 192.168.100.10 server1" /etc/hosts

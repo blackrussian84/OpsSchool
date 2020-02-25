a2754069dbd3ed34523d691d170665fef024bc17
Jenya <shwebsec@gmail.com>
2020-02-25 18:08:06 +0200

    no ex5

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-sudo apt-get install sshpass -y
-sudu apt-get install expect -y  
-ssh-keygen -t rsa
+
 
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-sudo apt-get install sshpass -y
-#sudo apt-get update 
-sudo echo -e "\n"|ssh-keygen -t rsa -N """" -f ~/vagrant/.ssh/id_rsa &&  sleep 1 printf '%s\n' y
 
-sudo sshpass -p "vagrant" ssh-copy-id -i ~/.ssh/id_rsa.pub 192.168.100.10 &

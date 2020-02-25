399d8e93fe2d9715f92be85ffa1095552d68be60
Jenya <shwebsec@gmail.com>
2020-02-26 00:09:32 +0200

    add ex 5 into ex4 for test

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
 sudo chmod o+w /etc/hosts && sed -i  "3i 192.168.100.11 server2" /etc/hosts
+
+sudo apt-get install -f sshpass
+
+#
+sudo su - vagrant -c "ssh-keygen -t rsa -N '' -q -f /home/vagrant/.ssh/id_rsa "
+sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh && chmod 0600 ~/.ssh/authorized_keys"
+
+sudo su - vagrant -c echo "Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null" >> /home/vagrant/.ssh/config
+
+sudo su - vagrant -c "chown vagrant:vagrant  /home/vagrant/.ssh/config"
+
+
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -2,3 +2,18 @@
 #add fix to exercise4-server2 here
 #sudo chmod o+w /etc/hosts && echo "192.168.100.10 server1" >> /etc/hosts
 sudo chmod o+w /etc/hosts && sed -i  "3i 192.168.100.10 server1" /etc/hosts
+
+
+
+sudo apt-get install -f sshpass
+
+# 
+sudo su - vagrant -c "ssh-keygen -t rsa -N '' -q -f /home/vagrant/.ssh/id_rsa "
+sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh && chmod 0600 ~/.ssh/authorized_keys"
+
+sudo su - vagrant -c echo "Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null" >> /home/vagrant/.ssh/config
+
+sudo su - vagrant -c "chown vagrant:vagrant  /home/vagrant/.ssh/config"
+

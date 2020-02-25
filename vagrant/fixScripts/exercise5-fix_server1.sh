b4b2e7c89b52e02099b361f401cf08d2d4d7cd0c
Jenya <shwebsec@gmail.com>
2020-02-25 23:34:18 +0200

    stam try

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
 
-sudo su - vagrant -c "ssh-keygen -t rsa -f /vagrant/id_rsa -q -N ''"
-sudo su - vagrant -c "cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa"
-sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh/id_rsa"
-sudo su - vagrant -c "cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys"
-sudo su - vagrant -c "chmod 0600 ~/.ssh/authorized_keys"
-echo "Host *" >> /home/vagrant/.ssh/config
-echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
-echo "UserKnownHostsFile=/dev/null" >> /home/vagrant/.ssh/config 
 
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-#add fix to exercise5-server2 here
-sudo chmod 700 ~/vagrant/.ssh/
-sudo chmod 644 ~/vagrant/.ssh/authorized_keys
-sudo echo "Host *
-    StrictHostKeyChecking no
-    UserKnownHostsFile /dev/null" > ~/vagrant/.ssh/config
-
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWccD6M+biUCAWs9YgBINKjEZiGmLYHqLAT8Drw70Z/Sd3UEjDhE+HtEVPUqNt2L6S8wnAOohfaKvkzbLqfrTj/kj2I4lallMVW8xyHEeOb+jQT6bVpSXHzwP95cILZubkRDtxRL1OFiRSdNIvJBYvkDkrx9PD/JJSbnenBJ2c/Wl993Kd6kPyaGi4wYB8o9LJUEu6e0WgGNwFgfQzbZGOUphNs60ujEqfMOjIsgugazc8zDMbXtgP+8whJECWKUKUK7S2aBViI8lzeyjcoCrg3/oAVXV0wOVq3FWoaRDAFrgm/M0ZZZmpvTU+29KxzAvdlICXyve0KQhOLvkLc8SX vagrant@server1"  | tee -a ~/vagrant/.ssh/authorized_keys
 

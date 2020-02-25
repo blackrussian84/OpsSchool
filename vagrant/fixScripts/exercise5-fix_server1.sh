a13b069fef2f5ea9ee520288f248ef78c3a6aa91
Jenya <shwebsec@gmail.com>
2020-02-25 17:58:27 +0200

    download expect , try to just keygen

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
 sudo apt-get install sshpass -y
-#sudo apt-get update 
-cd /home/vagrant && ssh-keygen -t rsa -N """" -f /.ssh | printf '%s\n' y
-
+sudu apt-get install expect -y  
+ssh-keygen -t rsa
 

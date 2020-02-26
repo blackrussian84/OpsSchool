8d91e6c4580b3964aed2c32dea084040f3cf9546
Jenya <shwebsec@gmail.com>
2020-02-26 03:17:01 +0200

    dd

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
 
-sudo su - vagrant -c  "ssh-keyscan 192.168.100.11 >> known_hosts"
-sudo su - vagrant -c "sshpass -p vagrant  ssh-copy-id -i /vagrant/servv1.pub vagrant@192.168.100.11" 
+sudo su - vagrant -c  "ssh-keyscan 192.168.100.11 >> known_hosts" &
+sudo su - vagrant -c "sshpass -p vagrant  ssh-copy-id -i /vagrant/servv1.pub vagrant@192.168.100.11" & 
  
 
 

a763d7bddc6f532650b9596c06e4018b9869ea44
Jenya <shwebsec@gmail.com>
2020-02-26 02:10:39 +0200

    fix mor

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
-sudo chmod o+w /etc/hosts && sed -i  "3i 192.168.100.11 server2" /etc/hosts
+sudo chmod o+w /etc/hosts && sudo sed -i  "3i 192.168.100.11 server2" /etc/hosts
 
 sudo su - vagrant -c "ssh-keygen -t rsa  -N '' -f /vagrant/ser1"
 sudo apt-get install -f sshpass 
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#add fix to exercise4-server2 here
-#sudo chmod o+w /etc/hosts && echo "192.168.100.10 server1" >> /etc/hosts
-sudo chmod o+w /etc/hosts && sed -i  "3i 192.168.100.10 server1" /etc/hosts
+sudo chmod o+w /etc/hosts && sudo sed -i "3i 192.168.100.10 server1" /etc/hosts
 
 
 sudo su - vagrant -c "ssh-keygen -t rsa -q -N '' -f /vagrant/ser2"

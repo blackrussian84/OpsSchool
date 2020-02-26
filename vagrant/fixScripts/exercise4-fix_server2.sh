90991268dba58cb89593a442a6f821cb1a3e41b9
Jenya <shwebsec@gmail.com>
2020-02-26 02:29:16 +0200

    ll

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -2,7 +2,7 @@
 sudo chmod o+w /etc/hosts && sudo sed -i "3i 192.168.100.10 server1" /etc/hosts
 
 
-sudo su - vagrant -c "ssh-keygen -t rsa  -N '' -f /vagrant/ser2"
+sudo  "ssh-keygen -t rsa  -N '' -f /vagrant/ser2"
 
 sudo apt-get install -f sshpass 
 

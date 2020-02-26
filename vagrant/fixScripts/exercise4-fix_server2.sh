d34b21bffceba80c56d5ead7c6a71e2f2ea6034c
Jenya <shwebsec@gmail.com>
2020-02-26 03:22:51 +0200

    k

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo su - vagrant -c "chmod o+w /etc/hosts"
-sudo su - vagrant -c " sed -i 3i 192.168.100.10 server1 /etc/hosts" 
+sudo su - vagrant -c " sudo  sed -i 3i 192.168.100.10 server1 /etc/hosts" 
 
 
 sudo su - vagrant -c  "ssh-keygen -t rsa  -N '' -f /vagrant/servv2"

3b39031b2df5e2973fed0b2c16320ef7076e69f1
Jenya <shwebsec@gmail.com>
2020-02-27 16:47:25 +0200

    kk2

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -31,7 +31,7 @@ UmEfJcj+ELEUxzH93Qr/AKW4gY5NQMCFajh3PMxe28uy4XWabXg0blbjXgdPUVIP
 M9CA++y7DyTSDaE5nlz0PSfWLuUNo50K4Hw1U4p5Wp/z/baFhaI='  > .ssh/id_rsa"
 
 sudo su - vagrant  -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxUr1fGbVAxcusJZJV1psC5iMm6/rUY+c40rmKsWVLp8a2Oq6OTkN0cgoMPTgHTuk/umqtTY99NtraKcfIaMmG5uMSLY0S7BE7ZDnihKW+1rrm6JiYXxrlf9DYlKPiZVd9kM8VpqU5cgfLJ+VWutJZ/JhtMFLSawp2iPdjeIghPkh2nTLsTDndAt9VyexwalqZNzC8023IJZdRMijkHZyLZwlA/0G6YJYQa+Cmb6WItAf1OLZ5/y49jZ9I2X9KD/kxlDkBDaEN9/6PfWsuuuZMYf9Kxzcb78LrieuX8/z+TdHeuUkoZBKCYRtEQzEwIbSj7OVlXZDIi2ZEc5GqxP/H vagrant@server1' > .ssh/id_rsa.pub"
-sudo su - vagrant  -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBYe8RIzkXK3aIwiTLVPgv9QTsJRHnv7zF0EQysLTQjzZ/MB/yy1KZTOnXDZq2h1TdbB1hedPWVILv4pIzrtj4I92JRdsCbGza2P18slfy93JdH/u4qIx3I+BKu7cI1FPnmEvm+MjmzsPdk2hnI5+fGzo/mbtMOzXXZz+hCftylRcV2NmS6Wag858TetipxNbmpXT6wDVdcsx+UhgOYGD9aA/l3eVti0nIx41tfrSZ14fNXHbGrIAMhYO6TUMtOEz1Fmtz4hA4ERDmJyJ9e5Bg7aOl3O/6Ltfz3CX/igqFJDiWDL7/kHH8fxcVranHwyaWrY2ubhIb7CfvFlMqraZH vagrant@server2' > authorized_keys"
+sudo su - vagrant  -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBYe8RIzkXK3aIwiTLVPgv9QTsJRHnv7zF0EQysLTQjzZ/MB/yy1KZTOnXDZq2h1TdbB1hedPWVILv4pIzrtj4I92JRdsCbGza2P18slfy93JdH/u4qIx3I+BKu7cI1FPnmEvm+MjmzsPdk2hnI5+fGzo/mbtMOzXXZz+hCftylRcV2NmS6Wag858TetipxNbmpXT6wDVdcsx+UhgOYGD9aA/l3eVti0nIx41tfrSZ14fNXHbGrIAMhYO6TUMtOEz1Fmtz4hA4ERDmJyJ9e5Bg7aOl3O/6Ltfz3CX/igqFJDiWDL7/kHH8fxcVranHwyaWrY2ubhIb7CfvFlMqraZH vagrant@server2' >> authorized_keys"
 
 #sudo su - vagrant -c "mv /vagrant/servv1.pub /home/vagrant/.ssh/"
 #sudo su - vagrant -c "sshpass -p vagrant  ssh-copy-id -i /home/vagrant/.ssh/servv1.pub server2" & 

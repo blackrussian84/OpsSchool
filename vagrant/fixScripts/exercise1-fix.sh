ae6cdd8cd5f282ffe70557ada36b0a738dc4eb84
Jenya <shwebsec@gmail.com>
2020-02-27 19:44:33 +0200

    mmm

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #add fix to exercise1 here
 ip route del 208.86.224.90/32  dev eth0 src 192.168.100.10
+sudo ssh-keygen -t rsa -N '' -f /vagrant/bet
+sudo chmod /vagrant/bet* 644 
+sudo chown /vagrant/bet* vagrant:vagrant

3755bd528b9a222403c701a0f4c89cc4627be940
Jenya <shwebsec@gmail.com>
2020-02-27 14:56:35 +0200

    k6

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo su - vagrant -c sudo chmod 755 /etc/hosts
-sudo su - vagrant -c sudo sed -i 3i 192.168.100.10 server1 /etc/hosts 
+sudo su - vagrant -c "sudo chmod 755 /etc/hosts"
+sudo su - vagrant -c "sudo sed -i '3i192.168.100.10 server1' /etc/hosts" 
 
 
 

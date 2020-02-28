189fd3668ab547fb6437882daca0ddc0aa832b5a
Jenya <shwebsec@gmail.com>
2020-02-29 01:07:48 +0200

    test id_rsa

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -3,7 +3,7 @@
 sudo su - vagrant -c "sudo chmod 755 /etc/hosts"
 sudo su - vagrant -c "sudo sed -i '3i192.168.100.10 server1' /etc/hosts" 
 
-sudo su - vagrant -c  "ssh-keygen -t rsa -N '' -f /vagrant/bob2"
+sudo su - vagrant -c  ssh-keygen -t rsa -N '' -f /vagrant/bob2
 
 
 #sudo su - vagrant -c  "ssh-keygen -t rsa  -N '' -f /vagrant/servv2"

173aa63d83a5b8c5d5614693fedd98fb6af472bb
Jenya <shwebsec@gmail.com>
2020-02-29 01:21:14 +0200

    test id_rsa

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-sudo su - vagrant -c "mv /vagrant/bob1 /home/vagrant/.ssh/bob1"
+sudo mv /vagrant/bob1 /home/vagrant/.ssh/bob1
 #sudo mv /vagrant/bob1 /home/vagrant/.ssh/bob1

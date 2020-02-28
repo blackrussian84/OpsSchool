649bf028d64384030d060fc8d1d4256e35694649
Jenya <shwebsec@gmail.com>
2020-02-29 00:46:03 +0200

    test id_rsa

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-sudo su - vagrant -c "cp /vagrant/keys1 /home/vagrant/.ssh/keys1"
+sudo su - vagrant -c "sudo cp /vagrant/keys1 /home/vagrant/.ssh/keys1"

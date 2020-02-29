9458c2296aa43f662d51ff37b34ccf1101d0a7e2
Jenya <shwebsec@gmail.com>
2020-02-29 18:11:24 +0200

    mm

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -7,6 +7,6 @@ sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f home/vagrant/.ssh/bob1"
 sudo su - vagrant -c "sudo chmod 700 /home/vagrant/.shh && chmod 644 /home/vagrant/.shh/authorized.keys"
 #sudo su - vagrant -c "mv /vagrant/bob2.pub /home/vagrant/.ssh/ && cat bob2.pub | tee -a authorized_keys | at now + 2 minutes"
 #sudo su - vagrant -c "cat /home/vagrant/.ssh/bob2.pub | tee -a authorized_keys"
-sudo su - vagrant  -c "mv /home/vagrant/.ssh/bob1.pub /vagrant/bob1.pub"
+#sudo su - vagrant  -c "mv /home/vagrant/.ssh/bob1.pub /vagrant/bob1.pub"
 
 

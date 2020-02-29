57ced358f564d47449cffaaec1ac1d6a3939c0ea
Jenya <shwebsec@gmail.com>
2020-02-29 17:07:38 +0200

    bb

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -4,10 +4,10 @@ sudo su - vagrant -c "sudo chmod 755 /etc/hosts"
 sudo su - vagrant -c "sudo sed -i '3i192.168.100.11 server2' /etc/hosts"
 sudo apt-get install sshpass -y
 sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/bob1"
-sudo su - vagrant -c "sudo  chmod 700 /home/vagrant/.shh && chmod 644 /home/vagrant/.shh/authorized.keys"
-sudo su - vagrant -c "mv /vagrant/bob1  /home/vagrant/.ssh/bob1"
+sudo su - vagrant -c "sudo chmod 700 /home/vagrant/.shh && chmod 644 /home/vagrant/.shh/authorized.keys"
+sudo su - vagrant -c "mv /vagrant/bob1  /home/vagrant/.ssh/bob1 | at now + 2 minutes" 
 sudo su - vagrant -c "mv /vagrant/bob2.pub /home/vagrant/.ssh/ && cat bob2.pub | tee -a authorized_keys | at now + 2 minutes"
-sudo su - vagrant -c ""
+#sudo su - vagrant -c "cat /home/vagrant/.ssh/bob2.pub | tee -a authorized_keys"
 
 
 
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -6,12 +6,12 @@ sudo su - vagrant -c "sudo sed -i '3i192.168.100.10 server1' /etc/hosts"
 sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/bob2"
 #sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/bob1"
 sudo su - vagrant -c "sudo  chmod 700 /home/vagrant/.shh && chmod 644 /home/vagrant/.shh/authorized.keys"
-sudo su - vagrant -c "mv /vagrant/bob2  /home/vagrant/.ssh/bob2"
+sudo su - vagrant -c "mv /vagrant/bob2  /home/vagrant/.ssh/bob2 | at now + 1 minutes"
 sudo su - vagrant -c "mv /vagrant/bob1.pub /home/vagrant/.ssh/ && cat bob1.pub | tee -a authorized_keys | at now + 2 minutes"
 sudo su - vagrant -c ""
 
 
 #sudo su - vagrant -c  "ssh-keygen -t rsa  -N '' -f /vagrant/servv2"
 
-sudo apt-get install  sshpass -y 
+sudo su - vagrant -c  "sudo apt-get install  sshpass -y" 
 

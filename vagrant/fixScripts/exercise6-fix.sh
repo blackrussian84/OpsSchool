3ec2d1ee478f2813bde628cc154e5293eb64d796
Jenya <shwebsec@gmail.com>
2020-02-29 23:08:54 +0200

    ex 6 done

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+
+
+num_of_args="$#"
+run_on=`hostname`
+local_file="${1}"
+local_file2="${2}"
+remote_dir="${3}"
+remote_host=""
+
+if [ $run_on == server2 ]; then 
+	remote_host="server1"
+else
+	remote_host="server2"
+fi
+ 
+if [ $# -eq 3 ]; then 
+	scp ${1} ${2} $remote_host:${3} > /dev/null 2>&1 
+        du -lch ${1} ${2} | awk ' END {print $1,$2}' 
+
+
+
+fi
+

036fc04d41171e29a1f2e742701d14be5c302d7a
Jenya <shwebsec@gmail.com>
2020-02-29 19:20:11 +0200

    f

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -2,3 +2,13 @@
 #add fix to exercise5-server1 here
 #sudo mv /vagrant/bob1 /home/vagrant/.ssh/bob1
 #sudo mv /vagrant/bob1 /home/vagrant/.ssh/bob1
+sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/id_rsa1"
+sudo su - vagrant -c "cp /vagrant/id_rsa1 /home/vagrant/.ssh/id_rsa1"
+sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh/id_rsa1"
+sleep 10
+sudo su - vagrant -c "cat /vagrant/id_rsa2.pub >> ~/.ssh/authorized_keys"
+sudo su - vagrant -c "chmod 0600 ~/.ssh/authorized_keys"
+sudo su - vagrant -c "echo Host * >> /home/vagrant/.ssh/config"
+sudo so - vagrant -c "echo StrictHostKeyChecking no >> /home/vagrant/.ssh/config"
+sudo su - vagrant -c "echo UserKnownHostsFile=/dev/null >> /home/vagrant/.ssh/config"
+
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo su - vagrant -c "ssh-keygen -t rsa -f /vagrant/id_rsa -q -N ''"
-sudo su - vagrant -c "cp /vagrant/id_rsa /home/vagrant/.ssh/id_rsa"
-sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh/id_rsa"
-sudo su - vagrant -c "cat /vagrant/id_rsa.pub >> ~/.ssh/authorized_keys"
+sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/id_rsa2"
+sudo su - vagrant -c "cp /vagrant/id_rsa1 /home/vagrant/.ssh/id_rsa2"
+sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh/id_rsa2"
+sleep 10
+sudo su - vagrant -c "cat /vagrant/id_rsa1.pub >> ~/.ssh/authorized_keys"
 sudo su - vagrant -c "chmod 0600 ~/.ssh/authorized_keys"
-echo "Host *" >> /home/vagrant/.ssh/config
-echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
-echo "UserKnownHostsFile=/dev/null" >> /home/vagrant/.ssh/confi
+sudo su - vagrant -c "echo Host * >> /home/vagrant/.ssh/config"
+sudo so - vagrant -c "echo StrictHostKeyChecking no >> /home/vagrant/.ssh/config"
+sudo su - vagrant -c "echo UserKnownHostsFile=/dev/null >> /home/vagrant/.ssh/config"

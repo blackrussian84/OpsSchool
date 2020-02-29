d140e6d37282d80b98deef81f6bbf1e27f97bec4
Jenya <shwebsec@gmail.com>
2020-02-29 21:28:23 +0200

    vvv

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -7,9 +7,8 @@ sudo su - vagrant -c "sudo apt-get install sshpass -y"
 sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/id_rsa2"
 sudo su - vagrant -c "cp /vagrant/id_rsa2 /home/vagrant/.ssh/id_rsa2"
 sudo su - vagrant -c "chmod 0600 /home/vagrant/.ssh/id_rsa2"
-sleep 10
-sudo su - vagrant -c "cp .ssh/id_rsa2 .shh/id_rsa2"
-sudo su - vagrant -c "mv .ssh/id_rsa2 .ssh/id_rsa" 
+sleep 5
+sudo su - vagrant -c "sudo mv .ssh/id_rsa2 .ssh/id_rsa" 
 sudo su - vagrant -c "cp /vagrant/id_rsa2.pub  /home/vagrant/.ssh/id_rsa.pub"
 sudo su - vagrant -c "cat /vagrant/id_rsa1.pub >> ~/.ssh/authorized_keys"
 sudo su - vagrant -c "chmod 0600 ~/.ssh/authorized_keys"
@@ -17,6 +16,7 @@ sudo su - vagrant -c "echo Host * >> /home/vagrant/.ssh/config"
 sudo su - vagrant -c "echo StrictHostKeyChecking no >> /home/vagrant/.ssh/config"
 sudo su - vagrant -c "echo UserKnownHostsFile=/dev/null >> /home/vagrant/.ssh/config"
 
-sudo su - vagrant -c "sshpass -p 'vagrant'  ssh-copy-id -i .ssh/id_rsa_pub server1"
-sudo su - vagrant -c "sshpass -p 'vagrant' ssh server1 cat /vagrant/id_rsa.pub2 | tee -a .ssh/authorized_keys && ssh-copy-id -i /vagrant/id_rsa1.pub server2"
+sudo su - vagrant -c "sshpass -p 'vagrant'  ssh-copy-id -i /home/vagrant/.ssh/id_rsa_pub server1"
+#sudo su - vagrant -c "sshpass -p 'vagrant' ssh server1 cat /vagrant/id_rsa.pub2 | tee -a .ssh/authorized_keys && ssh-copy-id -i /vagrant/id_rsa1.pub server2"
+sudo su - vagrant -c "sshpass -p 'vagrant' ssh server1 cat .ssh/id_rsa1.pub | tee -a .ssh/authorized_keys"
 

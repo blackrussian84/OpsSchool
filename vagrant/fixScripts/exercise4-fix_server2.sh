bb6bf067752109790a16da7c7ca953fcdce6aff5
Jenya <shwebsec@gmail.com>
2020-02-29 17:27:57 +0200

    ff

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -3,12 +3,11 @@
 sudo su - vagrant -c "sudo chmod 755 /etc/hosts"
 sudo su - vagrant -c "sudo sed -i '3i192.168.100.10 server1' /etc/hosts" 
 
-sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/bob2"
-#sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /vagrant/bob1"
+sudo su - vagrant -c "ssh-keygen -t rsa -N '' -f /home/vagrant/bob2"
 sudo su - vagrant -c "sudo  chmod 700 /home/vagrant/.shh && chmod 644 /home/vagrant/.shh/authorized.keys"
-sudo su - vagrant -c "mv /vagrant/bob2  /home/vagrant/.ssh/bob2 | at now + 1 minutes"
-sudo su - vagrant -c "mv /vagrant/bob1.pub /home/vagrant/.ssh/ && cat bob1.pub | tee -a authorized_keys | at now + 2 minutes"
-sudo su - vagrant -c ""
+sudo su - vagrant -c "mv /home/vagrant/.ssh/bob2.pub  /vagrant/bob2.pub"
+#sudo su - vagrant -c "mv /vagrant/bob1.pub /home/vagrant/.ssh/ && cat bob1.pub | tee -a authorized_keys | at now + 2 minutes"
+#sudo su - vagrant -c ""
 
 
 #sudo su - vagrant -c  "ssh-keygen -t rsa  -N '' -f /vagrant/servv2"
